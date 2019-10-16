9080cc520822c3c554c84c404cea8eafd3e51cda
Jaime Gomes <jaime.gomes@calipo.io>
2019-10-16 14:42:24 +0200

    add hex package information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Gira.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package,
     ]
   end
 
@@ -25,6 +26,15 @@ defmodule Gira.MixProject do
       {:httpoison, "~> 1.6"},
       {:poison, "~> 4.0"},
       {:jason, "~> 1.1"},
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README", "LICENSE*"],
+      maintainers: ["Jaime Gomes"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/fargozhu/gira"}
     ]
   end
 end
